3910a4b0a09b79d5b7d560600a0f928cb9891772
benzocv <anzarahmedkhan99@gmail.com>
2021-06-20 11:50:50 +0530

    crusd added

---
+++ 06-crud/tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `crud` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
