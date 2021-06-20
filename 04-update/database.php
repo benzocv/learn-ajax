4c640220fcea2e7836c3d3b9b5d2213541efe365
benzocv <anzarahmedkhan99@gmail.com>
2021-06-20 11:37:48 +0530

    update operation completed

--- 04-update/database.php
+++ 04-update/database.php
@@ -2,7 +2,7 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$db="school";
+	$db="sample5";
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password,$db);
 ?>
